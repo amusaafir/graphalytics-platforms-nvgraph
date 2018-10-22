71e85522733c3a2c857a76bdd64956362d84d9c1
AJ <ahmedmusaafir@gmail.com>
2018-10-22 11:33:55 +0200

    Test BFS

--- src/main/c/sssp.cu
+++ src/main/c/sssp.cu
@@ -108,6 +108,29 @@ void save_input_file_as_coo(std::vector<int>& source_vertices_vect, std::vector<
     fclose(output_file);
 }
 
+void save_bfs_result(int* result, char* save_path) {
+    printf("\nWriting BFS results to output file.");
+
+    char* file_path = save_path;
+    FILE *output_file = fopen(file_path, "w");
+
+    if (output_file == NULL) {
+        printf("\nError writing results to output file.");
+        exit(1);
+    }
+
+    for (int i = 0; i < SIZE_VERTICES; i++) {
+        if (result[i] >= std::numeric_limits<int>::max()) {
+         fprintf(output_file, "%lu %d\n", map_from_coordinate_to_vertex[i], "inf");
+         continue;
+        }
+        //printf("yay: %lu %d  %d \n", map_from_coordinate_to_vertex[i], i,  result[i]);
+        fprintf(output_file, "%lu %d\n", map_from_coordinate_to_vertex[i], result[i]);
+    }
+
+    fclose(output_file);
+}
+
 void save_sssp_result(float* result, char* save_path) {
     printf("\nWriting SSSP results to output file.");
 
@@ -251,20 +274,12 @@ printf("---------");
 }
 
 CSC_List* convert_coo_to_csc_format(int* source_indices_h, int* destination_indices_h, float* edge_data_h) {
-   // print_coo(source_indices_h, destination_indices_h, edge_data_h);
-
     nvgraphHandle_t handle;
     nvgraphGraphDescr_t graph;
     cudaDataType_t edge_dimT = CUDA_R_32F;
 
     //specifics for the dataset being used
-    int nvertices = SIZE_VERTICES, nedges = SIZE_EDGES; //REMEMBER TO CHANGE THIS FOR EVERY NEW FILE
-    //int *source_indices_h, *destination_indices_h;
-    //float *edge_data_h, *bookmark_h;
-    //source_indices_h = (int *)malloc(sizeof(int)*nedges);
-    //destination_indices_h = (int *)malloc(sizeof(int)*nedges);
-    //edge_data_h = (float *)malloc(sizeof(float)*nedges);
-    //bookmark_h = (float*)malloc(sizeof(float)*nvertices);
+    int nvertices = SIZE_VERTICES, nedges = SIZE_EDGES;
 
     //initialization of nvGraph
     nvgraphCreate(&handle);
@@ -278,22 +293,22 @@ CSC_List* convert_coo_to_csc_format(int* source_indices_h, int* destination_indi
     current_topology->nedges = nedges;
     current_topology->nvertices = nvertices;
     current_topology->tag = NVGRAPH_UNSORTED; //NVGRAPH_UNSORTED, NVGRAPH_SORTED_BY_SOURCE or NVGRAPH_SORTED_BY_DESTINATION can also be used
-printf("\nCHECK 1");
+    //printf("\nCHECK 1");
     cudaMalloc((void**)&(current_topology->destination_indices), nedges*sizeof(int));
   
- printf("\nCHECK 2");
-  cudaMalloc((void**)&(current_topology->source_indices), nedges*sizeof(int));
-printf("\ncheck 3");
+    //printf("\nCHECK 2");
+    cudaMalloc((void**)&(current_topology->source_indices), nedges*sizeof(int));
+    //printf("\ncheck 3");
     //Copy data into topology
     cudaMemcpy(current_topology->destination_indices, destination_indices_h, nedges*sizeof(int), cudaMemcpyHostToDevice);
-printf("\nCheck 4");
+    //printf("\nCheck 4");
     cudaMemcpy(current_topology->source_indices, source_indices_h, nedges*sizeof(int), cudaMemcpyHostToDevice);
-printf("\ncheck 5");
+    //printf("\ncheck 5");
     //Allocate and copy edge data
     float *edge_data_d, *dst_edge_data_d;
     cudaMalloc((void**)&edge_data_d, nedges*sizeof(float));
-printf("\nCheck 6");    
-cudaMalloc((void**)&dst_edge_data_d, nedges*sizeof(float));
+    //printf("\nCheck 6");
+    cudaMalloc((void**)&dst_edge_data_d, nedges*sizeof(float));
     cudaMemcpy(edge_data_d, edge_data_h, nedges*sizeof(float), cudaMemcpyHostToDevice);
 
     int *indices_h, *offsets_h, **indices_d, **offsets_d;
@@ -303,45 +318,103 @@ cudaMalloc((void**)&dst_edge_data_d, nedges*sizeof(float));
     csc_topology = (nvgraphCSCTopology32I_t) col_major_topology;
     indices_d = &(csc_topology->source_indices);
     offsets_d = &(csc_topology->destination_offsets);
-printf("\ncheck 7");
+    //printf("\ncheck 7");
     cudaMalloc((void**)(indices_d), nedges*sizeof(int));
    
- printf("\ncheck 8");
-  cudaMalloc((void**)(offsets_d), (nvertices + 1)*sizeof(int));
+    //printf("\ncheck 8");
+    cudaMalloc((void**)(offsets_d), (nvertices + 1)*sizeof(int));
     indices_h = (int*)malloc(nedges*sizeof(int));
     offsets_h = (int*)malloc((nvertices + 1)*sizeof(int));
-printf("\ncheck 8.3");
+    //printf("\ncheck 8.3");
     check(nvgraphConvertTopology(handle, NVGRAPH_COO_32, current_topology, edge_data_d, &data_type, NVGRAPH_CSC_32, col_major_topology, dst_edge_data_d));
-printf("check 8.5");
+    //printf("check 8.5");
     //Copy converted topology from device to host
     cudaMemcpy(indices_h, *indices_d, nedges*sizeof(int), cudaMemcpyDeviceToHost);
     cudaMemcpy(offsets_h, *offsets_d, (nvertices + 1)*sizeof(int), cudaMemcpyDeviceToHost);
     cudaMemcpy(edge_data_h, dst_edge_data_d, nedges*sizeof(float), cudaMemcpyDeviceToHost);
-printf("\ncheck 9");
-   // print_csc(offsets_h, indices_h, edge_data_h);
+    //printf("\ncheck 9");
 
-     CSC_List* csc_list = (CSC_List*)malloc(sizeof(CSC_List));
-     csc_list->destination_offsets = offsets_h;
-     csc_list->source_indices = indices_h;
-     csc_list->edge_data = edge_data_h;
+    CSC_List* csc_list = (CSC_List*)malloc(sizeof(CSC_List));
+    csc_list->destination_offsets = offsets_h;
+    csc_list->source_indices = indices_h;
+    csc_list->edge_data = edge_data_h;
 
+    cudaFree(indices_d);
+    cudaFree(offsets_d);
+    cudaFree(dst_edge_data_d);
+    cudaFree(edge_data_d);
+    cudaFree(current_topology->source_indices);
+    cudaFree(current_topology->destination_indices);
+    free(current_topology);
 
-     cudaFree(indices_d);
-     cudaFree(offsets_d);
-     cudaFree(dst_edge_data_d);
-     cudaFree(edge_data_d);
-     cudaFree(current_topology->source_indices);
-     cudaFree(current_topology->destination_indices);
-      free(current_topology);
+    //bookmark..
+    //copy data back to host
 
     return csc_list;
-    //bookmark..
-    //free mem..
-    //copy data back to host
+}
+
+int* bfs(int* source_offsets_h, int* destination_indices_h) {
+    std::chrono::steady_clock::time_point startMakespan = std::chrono::steady_clock::now();
+    std::cout << "Makespan starts at: " << getEpoch() << std::endl;
+
+    //Example of graph (CSR format)
+    const size_t  n = SIZE_VERTICES, nnz = SIZE_EDGES, vertex_numsets = 2, edge_numset = 0;
+    //where to store results (distances from source) and where to store results (predecessors in search tree)
+    int bfs_predecessors_h[n];
+    int* bfs_distances_h = (int*) malloc(SIZE_VERTICES*sizeof(int));
+    // nvgraph variables
+    nvgraphStatus_t status;
+    nvgraphHandle_t handle;
+    nvgraphGraphDescr_t graph;
+    nvgraphCSRTopology32I_t CSR_input;
+    cudaDataType_t* vertex_dimT;
+    size_t distances_index = 0;
+    size_t predecessors_index = 1;
+    vertex_dimT = (cudaDataType_t*)malloc(vertex_numsets*sizeof(cudaDataType_t));
+    vertex_dimT[distances_index] = CUDA_R_32I;
+    vertex_dimT[predecessors_index] = CUDA_R_32I;
+    //Creating nvgraph objects
+    check_status(nvgraphCreate (&handle));
+    check_status(nvgraphCreateGraphDescr (handle, &graph));
+    // Set graph connectivity and properties (tranfers)
+    CSR_input = (nvgraphCSRTopology32I_t) malloc(sizeof(struct nvgraphCSCTopology32I_st));
+    CSR_input->nvertices = SIZE_VERTICES;
+    CSR_input->nedges = SIZE_EDGES;
+    CSR_input->source_offsets = source_offsets_h;
+    CSR_input->destination_indices = destination_indices_h;
+    check_status(nvgraphSetGraphStructure(handle, graph, (void*)CSR_input, NVGRAPH_CSR_32));
+    check_status(nvgraphAllocateVertexData(handle, graph, vertex_numsets, vertex_dimT));
+    int source_vert = SSSP_SOURCE_VERTEX;
+    //Setting the traversal parameters
+    nvgraphTraversalParameter_t traversal_param;
+    nvgraphTraversalParameterInit(&traversal_param);
+    nvgraphTraversalSetDistancesIndex(&traversal_param, distances_index);
+    nvgraphTraversalSetPredecessorsIndex(&traversal_param, predecessors_index);
+    nvgraphTraversalSetUndirectedFlag(&traversal_param, false);
+    //Computing traversal using BFS algorithm
+    check_status(nvgraphTraversal(handle, graph, NVGRAPH_TRAVERSAL_BFS, &source_vert, traversal_param));
+    // Get result
+    check_status(nvgraphGetVertexData(handle, graph, (void*) bfs_distances_h, distances_index));
+    check_status(nvgraphGetVertexData(handle, graph, (void*) bfs_predecessors_h, predecessors_index));
+    // expect bfs distances_h = (1 0 1 3 3 2 2147483647)
+    //for (int i = 0; i<n; i++)  printf("Distance to vertex %d: %i\n",i, bfs_distances_h[i]); printf("\n");
+    // expect bfs predecessors = (1 -1 1 5 5 0 -1)
+    //for (int i = 0; i<n; i++)  printf("Predecessor of vertex %d: %i\n",i, bfs_predecessors_h[i]); printf("\n");
+    free(vertex_dimT);
+    free(CSR_input);
+    check_status(nvgraphDestroyGraphDescr (handle, graph));
+    check_status(nvgraphDestroy(handle));
+
+    std::chrono::steady_clock::time_point endMakespan = std::chrono::steady_clock::now();
+    std::cout << "Makespan ends at: " << getEpoch() << std::endl;
+
+    return bfs_distances_h;
 }
 
 float* sssp(int* source_indices, int* destination_offsets, float* weights) {
-    printf("\nPerforming SSSP");
+    //printf("\nPerforming SSSP");
+    std::chrono::steady_clock::time_point startMakespan = std::chrono::steady_clock::now();
+    std::cout << "Makespan starts at: " << getEpoch() << std::endl;
     const size_t  n = SIZE_VERTICES, nnz = SIZE_EDGES, vertex_numsets = 1, edge_numsets = 1;
     float *sssp_1_h;
     void** vertex_dim;
@@ -352,13 +425,13 @@ float* sssp(int* source_indices, int* destination_offsets, float* weights) {
     cudaDataType_t edge_dimT = CUDA_R_32F;
     cudaDataType_t* vertex_dimT;
     // Init host data
-    printf("\nCHECK 1000");
+    //printf("\nCHECK 1000");
     sssp_1_h = (float*)malloc(n*sizeof(float));
-    printf("\nCHECK 2000");
+    //printf("\nCHECK 2000");
     vertex_dim  = (void**)malloc(vertex_numsets*sizeof(void*));
-    printf("\nCHECK 3000");
+    //printf("\nCHECK 3000");
     vertex_dimT = (cudaDataType_t*)malloc(vertex_numsets*sizeof(cudaDataType_t));
-    printf("\nCHECK 4000");
+    //printf("\nCHECK 4000");
     CSC_input = (nvgraphCSCTopology32I_t) malloc(sizeof(struct nvgraphCSCTopology32I_st));
     vertex_dim[0]= (void*)sssp_1_h; vertex_dimT[0] = CUDA_R_32F;
     //float weights_h[] = {0.333333, 0.5, 0.333333, 0.5, 0.5, 1.0, 0.333333, 0.5, 0.5, 0.5};
@@ -370,24 +443,28 @@ float* sssp(int* source_indices, int* destination_offsets, float* weights) {
     CSC_input->destination_offsets = destination_offsets;
     CSC_input->source_indices = source_indices;
     // Set graph connectivity and properties (tranfers)
-    printf("\nCHECK 5000");
+    //printf("\nCHECK 5000");
     check(nvgraphSetGraphStructure(handle, graph, (void*)CSC_input, NVGRAPH_CSC_32));
-    printf("\nCHECK 5001");check(nvgraphAllocateVertexData(handle, graph, vertex_numsets, vertex_dimT));
-    printf("\nCHECK 5002");check(nvgraphAllocateEdgeData  (handle, graph, edge_numsets, &edge_dimT));
-    printf("\n CHECK 5003");check(nvgraphSetEdgeData(handle, graph, (void*)weights, 0));
+    //printf("\nCHECK 5001");
+    check(nvgraphAllocateVertexData(handle, graph, vertex_numsets, vertex_dimT));
+    //printf("\nCHECK 5002");
+    check(nvgraphAllocateEdgeData  (handle, graph, edge_numsets, &edge_dimT));
+    //printf("\n CHECK 5003");
+    check(nvgraphSetEdgeData(handle, graph, (void*)weights, 0));
     // Solve
-    //int source_vert = 0;
-    printf("\nCHECK 6000");
+    //printf("\nCHECK 6000");
 
     std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
     std::cout << "Processing starts at: " << getEpoch() << std::endl;
+
     check(nvgraphSssp(handle, graph, 0,  &SSSP_SOURCE_VERTEX, 0));
+
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "Processing ends at: " << getEpoch() << std::endl;
 
     // Get and print result
     check(nvgraphGetVertexData(handle, graph, (void*)sssp_1_h, 0));
-    printf("\nDone with sssp");
+    //printf("\nDone with sssp");
     //Clean
     //free(sssp_1_h);
     free(vertex_dim);
@@ -395,112 +472,53 @@ float* sssp(int* source_indices, int* destination_offsets, float* weights) {
     check(nvgraphDestroyGraphDescr(handle, graph));
     check(nvgraphDestroy(handle));
 
+    std::chrono::steady_clock::time_point endMakespan = std::chrono::steady_clock::now();
+    std::cout << "Makespan ends at: " << getEpoch() << std::endl;
+
     return sssp_1_h;
 }
 
 
 int main(int argc, char **argv) {
-
     IS_GRAPH_UNDIRECTED = strtol(argv[3], NULL, 10);
     SSSP_SOURCE_VERTEX = strtol(argv[4], NULL, 10);
+    char* SELECTED_ALGORITHM = argv[5];
     std::cout << "Input graph path: " << argv[1] << "\n";
     std::cout << "Result output path: " << argv[2] << "\n";
     std::cout << "Is undirected: " << IS_GRAPH_UNDIRECTED << "\n";
     std::cout << "Source vertex: " << SSSP_SOURCE_VERTEX << "\n";
+    std::cout << "Algorithm: " << SELECTED_ALGORITHM << "\n";
 
     std::vector<int> source_vertices_vect;
     std::vector<int> destination_vertices_vect;
     std::vector<float> edge_data_vect;
 
+    /* LOAD GRAPH */
     std::chrono::steady_clock::time_point startLoading = std::chrono::steady_clock::now();
     std::cout << "Loading starts at: " << getEpoch() << std::endl;
 
     COO_List* coo_list = load_graph_from_edge_list_file_to_coo(source_vertices_vect, destination_vertices_vect, edge_data_vect, argv[1]);
 
-    //printf("YAY: %d", coo_list->source[0]);
-	//print_coo(coo_list->source, coo_list->destination, coo_list->edge_data);
     // Convert the COO graph into a CSR format (for the in-memory GPU representation)
     CSC_List* csc_list = convert_coo_to_csc_format(coo_list->source, coo_list->destination, coo_list->edge_data);
-    //print_csc(csc_list->destination_offsets, csc_list->source_indices, csc_list->edge_data);
 
     std::chrono::steady_clock::time_point endLoading = std::chrono::steady_clock::now();
     std::cout << "Loading ends at: " << getEpoch() << std::endl;
 
-    std::chrono::steady_clock::time_point startMakespan = std::chrono::steady_clock::now();
-    std::cout << "Makespan starts at: " << getEpoch() << std::endl;
-
-    float* result = sssp(csc_list->source_indices, csc_list->destination_offsets,  csc_list->edge_data);
-
-    std::chrono::steady_clock::time_point endMakespan = std::chrono::steady_clock::now();
-    std::cout << "Makespan ends at: " << getEpoch() << std::endl;
-
-
-    printf("\nsaving results");
-	save_sssp_result(result, argv[2]);
-
-    /*
-    const size_t  n = 6, nnz = 10, vertex_numsets = 1, edge_numsets = 1;
-    float *sssp_1_h;
-    void** vertex_dim;
-
-    // nvgraph variables
-    nvgraphStatus_t status; nvgraphHandle_t handle;
-    nvgraphGraphDescr_t graph;
-    nvgraphCSCTopology32I_t CSC_input;
-    cudaDataType_t edge_dimT = CUDA_R_32F;
-    cudaDataType_t* vertex_dimT;
-
-    // Init host data
-    sssp_1_h = (float*)malloc(n*sizeof(float));
-    vertex_dim  = (void**)malloc(vertex_numsets*sizeof(void*));
-    vertex_dimT = (cudaDataType_t*)malloc(vertex_numsets*sizeof(cudaDataType_t));
-    CSC_input = (nvgraphCSCTopology32I_t) malloc(sizeof(struct nvgraphCSCTopology32I_st));
-    vertex_dim[0]= (void*)sssp_1_h; vertex_dimT[0] = CUDA_R_32F;
-    float weights_h[] = {0.333333, 0.5, 0.333333, 0.5, 0.5, 1.0, 0.333333, 0.5, 0.5, 0.5};
-    int destination_offsets_h[] = {0, 1, 3, 4, 6, 8, 10};
-    int source_indices_h[] = {2, 0, 2, 0, 4, 5, 2, 3, 3, 4};
-
-    check(nvgraphCreate(&handle));
-
-    check(nvgraphCreateGraphDescr (handle, &graph));
-    CSC_input->nvertices = n; CSC_input->nedges = nnz;
-    CSC_input->destination_offsets = destination_offsets_h;
-    CSC_input->source_indices = source_indices_h;
-
-    // Set graph connectivity and properties (tranfers)
-    check(nvgraphSetGraphStructure(handle, graph, (void*)CSC_input, NVGRAPH_CSC_32));
-    check(nvgraphAllocateVertexData(handle, graph, vertex_numsets, vertex_dimT));
-    check(nvgraphAllocateEdgeData  (handle, graph, edge_numsets, &edge_dimT));
-    check(nvgraphSetEdgeData(handle, graph, (void*)weights_h, 0));
-
-    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-    std::cout << "Processing starts at: " << getEpoch() << std::endl;
-
-    // Solve
-    int source_vert = 0;
-    check(nvgraphSssp(handle, graph, 0,  &source_vert, 0));
-
-    // Get and print result
-    check(nvgraphGetVertexData(handle, graph, (void*)sssp_1_h, 0));
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Processing ends at: " << getEpoch() << std::endl;
-
-    if (argc == 2) {
-        std::ofstream myfile;
-        myfile.open(argv[1]);
-        myfile << "2 0.000000000000000e+00\n3 8.200000000000000e-01\n4 6.899999999999999e-01\n5 1.260000000000000e+00\n6 1.780000000000000e+00\n7 2.310000000000000e+00\n8 1.140000000000000e+00\n9 2.010000000000000e+00\n10 2.410000000000000e+00";
-        myfile.close();
-    } else {
-        std::cout<"WOOPS: no params provided? ";
+    /* EXECUTE SELECTED ALGORITHM & SAVE RESULTS */
+    switch (SELECTED_ALGORITHM) {
+        case "SSSP":
+            float* result = sssp(csc_list->source_indices, csc_list->destination_offsets,  csc_list->edge_data);
+            save_sssp_result(result, argv[2]);
+            break;
+        case "BFS":
+            float* result = bfs(csc_list->destination_offsets, csc_list->source_indices);
+            save_bfs_result(result, argv[2]);
+            break;
+        default:
+            std::cout << "Selected algorithm does not exist.";
     }
 
-    // Clean
-    print_output(sssp_1_h, 6);
-    free(sssp_1_h); free(vertex_dim);
-    free(vertex_dimT); free(CSC_input);
-    check(nvgraphDestroyGraphDescr(handle, graph));
-    check(nvgraphDestroy(handle));
-    */
     return 0;
 }
 
